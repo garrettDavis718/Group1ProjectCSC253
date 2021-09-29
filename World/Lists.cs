4b0bc5c29a08430e2cf740b7566aa07a5a4f2cab
garrett davis <gardongodavongo@gmail.com>
2021-09-29 13:23:33 -0400

    added world

---
+++ World/Lists.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World
+{
+    public static class Lists
+    {
+        public static List<PlayerCharacter> currentPlayer = new List<PlayerCharacter>();
+        public static List<Room> rooms = new List<Room>();
+        public static List<Mob> Mobs = new List<Mob>();
+        public static List<Treasure> Treasures = new List<Treasure>();
+        public static List<Potion> Potions = new List<Potion>();
+        public static List<Item> Items = new List<Item>();
+        public static List<Mob> CurrentEnemies = new List<Mob>();
+        public static List<Weapon> Weapons = new List<Weapon>();
+        public static List<Weapon> CurrentWeapon = new List<Weapon>();
+    }
+}
---
+++ World/Mob.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World
+{
+    //Mob class
+    public class Mob
+    {
+        //properties
+        private string _mobId;
+        private string _name;
+        private double _healthPoints;
+        private double _manaPoints;
+        private string _desc;
+        private double _armorClass;
+        private int _roomIndex;
+        
+        //constructor
+        public Mob(string mobId, string name, double healthPoints, double manaPoints, double armorClass, string desc, int roomIndex)
+        {
+            _mobId = mobId;
+            _name = name;
+            _healthPoints = healthPoints;
+            _manaPoints = manaPoints;
+            _desc = desc;
+            _armorClass = armorClass;
+            _roomIndex = roomIndex;
+        }
+        //default constructor
+        public Mob()
+        {
+            _mobId = MobId;
+            _name = Name;
+            _healthPoints = HealthPoints;
+            _manaPoints = ManaPoints;
+            _desc = Desc;
+            _armorClass = ArmorClass;
+            _roomIndex = RoomIndex;
+        }
+        //properties
+        public string MobId
+        {
+            get { return _mobId; }
+            set { _mobId = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public double HealthPoints
+        {
+            get { return _healthPoints; }
+            set { _healthPoints = value; }
+        }
+        public double ManaPoints
+        {
+            get { return _manaPoints; }
+            set { _manaPoints = value; }
+        }
+        public double ArmorClass
+        {
+            get { return _armorClass ; }
+            set { _armorClass = value; }
+        }
+        public string Desc
+        {
+            get { return _desc; }
+            set { _desc = value; }
+        }
+        public int RoomIndex
+        {
+            get { return _roomIndex; }
+            set { _roomIndex = value; }
+        }
+    }
+}
